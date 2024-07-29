fe7c31aacab9b5fb9bbbab2b32c9787fcb240129
yattishr <yattish@gmail.com>
2024-07-29 18:50:44 +0200

    fixing API key issue.

--- firebase.ts
+++ firebase.ts
@@ -6,7 +6,7 @@ import { getStorage } from "firebase/storage"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBU3QsFSqXvGnKZq3ZbiQzg3ind1aHDlmo",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "chat-with-pdf-f75c7.firebaseapp.com",
   projectId: "chat-with-pdf-f75c7",
   storageBucket: "chat-with-pdf-f75c7.appspot.com",
